5a045a0d079c130ad9e76ce0c1c3b0ddc055dc84
Josh Quaid <josh.quaid@gmail.com>
2022-03-15 17:20:02 -0500

    *Added reset password link 	*Set up the code to process a password reset request 	*Tested with a valid user 	*Set up code to only allow the send reset link button if a properly formatted email is entered into the text field. 	*Modifying the button handler to work with both buttons and provide different alertControllers based on button pressed.

--- Team_India/LoginViewController.swift
+++ Team_India/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailAddrField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var forgotButton: UIButton!
     
     // MARK: - View Lifecycles
     override func viewDidLoad() {
@@ -26,13 +27,25 @@ class LoginViewController: UIViewController {
 
     @IBAction func logInButtonHandler(_ sender: UIButton) {
         
+        switch sender {
+        case forgotButton :
+            showPassReset()
+            return
+        case logInButton :
+            //do login stuff
+            print("log in button")
+        default :
+            return
+        }
+        
+        
         // Get the text field data and verify they are non-empty
         guard let email = emailAddrField.text,
               !email.isEmpty,
               let password = passwordField.text,
               !password.isEmpty
         else {
-            // add code to highlight field(s) that's empty
+            showErrorMessage(message: "Please complete all fields.")
             return
         }
         
@@ -42,34 +55,75 @@ class LoginViewController: UIViewController {
             showErrorMessage(message: "Email is not formatted properly.")
             return
         } else {
-            // send email and password to authentication server and process
-            Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
-                
-                if error != nil {
-                    // Show the error from Firebase
-                    self?.showErrorMessage(message: error?.localizedDescription ?? "Unknown login error")
-                } else {
-                    // User succesfully logged in, go to Home
-                    self?.goToHome()
+            
+            if sender == logInButton {
+                // send email and password to authentication server and process
+                Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
+                    
+                    if error != nil {
+                        // Show the error from Firebase
+                        self?.showErrorMessage(message: error?.localizedDescription ?? "Unknown login error")
+                    } else {
+                        // User succesfully logged in, go to Home
+                        self?.goToHome()
+                    }
                 }
             }
         }
     }
-    
     
     // MARK: - Funcs
     
     // Shows error messages in an AlertController
     func showErrorMessage(message : String) {
         
-        // Show an AlertController
+        // Create an AlertController
         let alertController = UIAlertController(title: "Error Logging In",
                                           message: message,
                                                 preferredStyle: .alert)
         let okAction = UIAlertAction(title: "Ok", style: .default)
         // Add the action to the alertController
         alertController.addAction(okAction)
+        
+        // Show the alert controller
         self.present(alertController, animated: true)
+    }
+    
+    
+    // Show the password reset Alert Controller
+    func showPassReset() {
+        // Create an AlertController
+        let passAlertController = UIAlertController(title: "",
+                                                message: "Please enter your email to continue.",
+                                                preferredStyle: .alert)
+        // Add a text field to get the user's email address and add a callback to "alertTextFieldDidChange" to only enable the sendAction button if the email entered is formatted properly
+        passAlertController.addTextField { forEmail in
+            forEmail.placeholder = "Email address"
+            forEmail.addTarget(self, action: #selector(self.alertTextFieldDidChange), for: .editingChanged)
+        }
+        
+        // Set up the action buttons
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        // Set up the send reset link to call the sendPasswordReset method with the provided email address. We know there is an email if the button is enabled since we check with the callback for the forEmail text field.
+        let sendAction = UIAlertAction(title: "Send Reset Link", style: .default) { _ in
+            let email = passAlertController.textFields?[0].text
+            Auth.auth().sendPasswordReset(withEmail: email!) { errorResettingEmail in
+                if errorResettingEmail != nil {
+                    self.showErrorMessage(message: errorResettingEmail!.localizedDescription)
+                }
+            }
+        }
+        
+        // Add the actions to the alertController
+        passAlertController.addAction(cancelAction)
+        passAlertController.addAction(sendAction)
+        // Disable the send reset link action until user types a good email address
+        sendAction.isEnabled = false
+        
+        // Show the alert controller
+        self.present(passAlertController, animated: true)
+
+        
     }
     
     // Sends the user to the hoome page upon successful registration/log in
@@ -86,7 +140,25 @@ class LoginViewController: UIViewController {
         view.window?.makeKeyAndVisible()
     }
         
+       
+    // The func to check if a good email is entered into the password reset alertController
+    @objc func alertTextFieldDidChange(sender : UITextField) {
         
+        // Get the alertController, the email address field, and the send reset action button
+        guard let alertController = self.presentedViewController as? UIAlertController,
+              let emailField = alertController.textFields?[0],
+              let sendAction = alertController.actions.last else {
+            return
+        }
+        
+        // Only enable the send reset button if the email is formatted properly
+        if let emailAddress = emailField.text {
+            sendAction.isEnabled = FormValidationUtil.isEmailFormatted(emailField: emailAddress)
+        }
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
