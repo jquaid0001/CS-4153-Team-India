cb94d1f687ed09b5b57fa73b6ac0302edee5a518
Josh Quaid <josh.quaid@gmail.com>
2022-03-24 16:13:42 -0500

    *Keyboard flow control and secure password work 	*Keyboard control is passed to password field when "next" is tapped 	*Keyboard control is resigned and button action executed if LOG IN button is active 	*LOG IN button is not active unless both fields contain data 	*Password field now has visibility button and shows the password and hides on clicks 	*Need additional testing and cleanup

--- Team_India/LoginViewController.swift
+++ Team_India/LoginViewController.swift
@@ -8,30 +8,80 @@
 import UIKit
 import FirebaseAuth
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailAddrField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var forgotButton: UIButton!
-    
+
+    let visibilityButton = UIButton(type: .custom)
     
     // MARK: - View Lifecycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        visibilityButton.frame = CGRect(x: 0, y: 0, width: 50, height: passwordField.frame.height)
+        // Try to get the system image for eye slash and set the visibilityButton's image to it
+        if let visImage = UIImage(systemName: "eye.slash") {
+            visibilityButton.setImage(visImage, for: .normal)
+        }
+        visibilityButton.addTarget(self, action: #selector(self.togglePassVis), for: .touchUpInside)
+        let visButtonContainer = UIView(frame: visibilityButton.frame)
+        visButtonContainer.backgroundColor = .clear
+        visButtonContainer.addSubview(visibilityButton)
+
+        // Set the VC as the delegate for the textFields
+        emailAddrField.delegate = self
+        passwordField.delegate = self
+        // Set up the props for the textFields keyboards
+        emailAddrField.keyboardType = .emailAddress
+        emailAddrField.returnKeyType = .next
+        passwordField.returnKeyType = .done
+        // Set the visibility button to show on the right of passwordField
+        passwordField.rightViewMode = .always
+        passwordField.rightView = visButtonContainer
+        
+        // Set the LOG IN button's background color so it's visible even when disabled
+        logInButton.configuration?.background.backgroundColor = .systemBlue
+        // Disable the LOG IN button until fields are populated
+        logInButton.isEnabled = false
     }
     
 
+    // MARK: - Observers
+    
+    @IBAction func textFieldDidChange(_ sender: UITextField) {
+        
+        // If the password visibility is toggled, turn it off when user begins typing in the password field again
+        if sender == passwordField && !passwordField.isSecureTextEntry {
+            togglePassVis()
+        }
+        
+        // Get the fields and check if they are both not empty
+        guard let email = emailAddrField.text,
+              !email.isEmpty,
+              let passwordField = passwordField.text,
+              !passwordField.isEmpty
+        else {
+            // If any fields becomes empty or is empty, make sure the LOG IN button is disabled
+            logInButton.isEnabled = false
+            return
+        }
+        // If both fields contain data, enable the LOG IN button else, leave it disabled
+        logInButton.isEnabled = true
+    }
+    
     // MARK: - Handlers
-
+    
     @IBAction func buttonHandler(_ sender: UIButton) {
         
         switch sender {
         case forgotButton :
             showPassReset()
         case logInButton :
-            // Get the text field data and verify they are non-empty
+            // Get the text field data and verify they are non-nil and populated for safety
             guard let email = emailAddrField.text,
                   !email.isEmpty,
                   let password = passwordField.text,
@@ -65,6 +115,27 @@ class LoginViewController: UIViewController {
     }
     
     // MARK: - Funcs
+    
+    // Keyboard flow control for textFields
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // If user hits "next" in the emailAddrField, send the kayboard control to the passwordField
+        if textField == emailAddrField {
+            passwordField.becomeFirstResponder()
+            return true
+        } else {
+            // If both fields are populated, the LOG IN button will be enabled in the textFieldDidChange func, so we are good to go for log in.
+            if logInButton.isEnabled == true {
+                // Close the keyboard
+                passwordField.resignFirstResponder()
+                // Simulate the LOG IN button press if the done button is pressed on the keyboard
+                self.buttonHandler(logInButton)
+                return true
+            }
+            // If the LOG IN button isn't enabled, do nothing when the user taps the "done" button on the keyboard.
+            return false
+        }
+    }
     
     // Shows error messages in an AlertController
     func showErrorMessage(message : String) {
@@ -135,7 +206,6 @@ class LoginViewController: UIViewController {
        
     // The func to check if a good email is entered into the password reset alertController
     @objc func alertTextFieldDidChange(sender : UITextField) {
-        
         // Get the alertController, the email address field, and the send reset action button
         guard let alertController = self.presentedViewController as? UIAlertController,
               let emailField = alertController.textFields?[0],
@@ -149,5 +219,30 @@ class LoginViewController: UIViewController {
         }
         
     }
-
+    
+    @IBAction func togglePassVis() {
+        passwordField.isSecureTextEntry.toggle()
+        // Set the image to the opposite of what is displayed for the visibilityButton
+        if !passwordField.isSecureTextEntry {
+            if let visImage = UIImage(systemName: "eye") {
+                visibilityButton.setImage(visImage, for: .normal)
+            }
+        } else {
+            if let existingPassText = passwordField.text {
+                passwordField.deleteBackward()
+                if let passwordTextRange = passwordField.textRange(from: passwordField.beginningOfDocument, to: passwordField.endOfDocument) {
+                passwordField.replace(passwordTextRange, withText: existingPassText)
+                }
+            }
+            
+            if let visImage = UIImage(systemName: "eye.slash") {
+                visibilityButton.setImage(visImage, for: .normal)
+            }
+            
+            if let existingSelectedRange = passwordField.selectedTextRange {
+                passwordField.selectedTextRange = nil
+                passwordField.selectedTextRange = existingSelectedRange
+            }
+        }
+    }
 }
