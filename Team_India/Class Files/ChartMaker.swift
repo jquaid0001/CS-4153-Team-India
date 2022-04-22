d1cdd095c3cb226446e5371f8f2a20c960bf2f98
Josh Quaid <josh.quaid@gmail.com>
2022-04-22 17:46:36 -0500

    *Data cleanup and commenting of code in ChartMaker

--- Team_India/Class Files/ChartMaker.swift
+++ Team_India/Class Files/ChartMaker.swift
@@ -3,15 +3,17 @@
 //  Team_India
 //
 //  Created by Josh Quaid on 4/20/22.
+//  Written by Jordan Maples
 //
 
 import Foundation
 import UIKit
 import Charts
 
+// The class for making charts
 class ChartMaker {
     
-    
+    // Sets up the barChart and returns a built barChart
     static func makeBarChart() -> BarChartView {
         // Create bar chart
         let barChart = BarChartView()
@@ -52,18 +54,24 @@ class ChartMaker {
         l.font = UIFont(name: "HelveticaNeue-Light", size: 12)!
         l.xEntrySpace = 6
         
+        // Animates the barChart in the yAxis direction
         barChart.animate(yAxisDuration: 2.5)
+        
         
         return barChart
     }
     
+    // Sets up a LineChart and returns the chart
     static func makeLineChart() -> LineChartView {
         let lineChart = LineChartView()
         
+        // Set the background color
         lineChart.backgroundColor = .systemFill
         
+        // Disable the rightAxis so there is no displaying of labels on the right side
         lineChart.rightAxis.enabled = false
         
+        // Configure the yAxis settings for the lineChart
         let yAxis = lineChart.leftAxis
         yAxis.labelFont = .boldSystemFont(ofSize: 14)
         yAxis.setLabelCount(6, force: false)
@@ -72,14 +80,18 @@ class ChartMaker {
         yAxis.axisMinimum = 0
         yAxis.axisMaximum = 12
         
+        // Configure the xAxis settings for the lineChart
         lineChart.xAxis.labelPosition = .bottom
         lineChart.xAxis.labelFont = .boldSystemFont(ofSize: 14)
         lineChart.xAxis.setLabelCount(6, force: false)
         lineChart.xAxis.axisLineColor = .black
         lineChart.xAxis.gridColor = .black
         
+        // Animate the lineChart in the xAxis direction
         lineChart.animate(xAxisDuration: 2.5)
         
+        
+        // Static data entries as an example
         var entries = [ChartDataEntry]()
         
         for x in 0..<7 {
@@ -88,10 +100,14 @@ class ChartMaker {
                            )
         }
         
+        // Create a set of ChartDataEntries
         let set = LineChartDataSet(entries: entries, label: "Day")
+        // Set the colors for the lineChart
         set.colors = ChartColorTemplates.material()
 
+        // Create a LineChartData object
         let data = LineChartData(dataSet: set)
+        // Set the lineChart data to data
         lineChart.data = data
         
         return lineChart
